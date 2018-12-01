dfc98660574be996597460671557857689ba1acb
William del Rosario <williamdelrosario@yahoo.com>
2018-12-01 11:43:55 +0100

    doctrine extension sluggable

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="AppBundle\Repository\GenusRepository")
@@ -26,6 +27,7 @@ class Genus
     private $name;
 
     /**
+     * @Gedmo\Slug(fields={"name"})
      * @ORM\Column(type="string", unique=true)
      */
     private $slug;
@@ -152,7 +154,7 @@ class Genus
         $this->firstDiscoveredAt = $firstDiscoveredAt;
 
     }
-    
+
     /**
      * @return mixed
      */
